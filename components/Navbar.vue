ef9e7f2ca6d026737d541305bf79729d6af13fc0
HyungJu <sungkisa@naver.com>
2020-09-11 23:23:42 +0900

    feat: nuxtjs and Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav
+    class="flex flex-row items-center justify-between bg-alimiblack py-5 px-10 border-b-2 border-gray-800"
+  >
+    <div class="flex text-white">
+      <p class="text-lg" style="font-family: GmarketSansBold, sans-serif">
+        E알리미
+      </p>
+    </div>
+    <div
+      class="flex flex-row text-white"
+      style="font-family: GmarketSansMedium, sans-serif"
+    >
+      <div class="ml-4 text-gray-600">받은 알리미</div>
+      <div class="ml-4">학급 알리미</div>
+      <div class="ml-4 text-gray-600">도움말</div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'GmarketSansBold';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansBold.woff')
+    format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'GmarketSansMedium';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff')
+    format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        alimiblack: '#1C1C24',
+      },
+      spacing: {
+        96: '24rem',
+        128: '32rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
